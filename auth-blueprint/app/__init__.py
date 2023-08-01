41e12d744c3f75a2077953fc773fe37561c185ae
your username <decentweb2018@gmail.com>
2023-08-01 16:24:30 -0700

    login and register routes

--- auth-blueprint/app/__init__.py
+++ auth-blueprint/app/__init__.py
@@ -2,14 +2,16 @@ from flask import Flask
 from flask_migrate import Migrate  
 from .main.models import db
 from .main.routes import main
+from flask_jwt_extended import JWTManager
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:8889/goalie2'
+    app.config['JWT_SECRET_KEY'] = 'your-secret-key'  # replace with your secret key
     
     db.init_app(app)
-
     migrate = Migrate(app, db)  
+    JWTManager(app)  # initialize JWTManager with the Flask app
 
     app.register_blueprint(main)
 
--- auth-blueprint/app/main/routes.py
+++ auth-blueprint/app/main/routes.py
@@ -1,17 +1,42 @@
-from flask import Blueprint, request
-from .models import User, db
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token, jwt_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import db, User
 
 main = Blueprint('main', __name__)
 
-@main.route('/register', methods=['POST'])
+@main.route('/api/register', methods=['POST'])
 def register():
     data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
-    if not username or not password:
-        return {"error": "Data not valid"}, 400
-    new_user = User(username=username)
-    new_user.set_password(password)
+    username = data['username']
+    password = data['password']
+
+    user = User.query.filter_by(username=username).first()
+
+    if user:
+        return jsonify(message="User already exists."), 400
+
+    new_user = User(username=username, password_hash=generate_password_hash(password, method='sha256'))
     db.session.add(new_user)
     db.session.commit()
-    return {"message": "User created"}, 201
+
+    return jsonify(message="User created."), 200
+
+@main.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user or not check_password_hash(user.password_hash, password):
+        return jsonify(message="Bad credentials."), 401
+
+    token = create_access_token(identity=username)
+    return jsonify(token=token), 200
+
+@main.route('/api/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    return jsonify(message="This is a protected endpoint."), 200
